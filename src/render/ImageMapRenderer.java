15e44693b4097dc4d5d25c585a0d6230a1863059
StylexTV <56502218+StylexTV@users.noreply.github.com>
2020-01-13 16:13:04 +0100

    Update 2.3
    Former-commit-id: bb0aca527faa4198b06291ee4fc1a4ce683f49c4 [formerly d7076745fda14c9b432850b74f61c75266c0a1bd] [formerly 67c1c919da26a38d710dc78416a7f9a7cfbd6857 [formerly 4b796dacc3e6ffee2430709d270904f5c7beb806 [formerly 4b796dacc3e6ffee2430709d270904f5c7beb806 [formerly 4b796dacc3e6ffee2430709d270904f5c7beb806 [formerly fe58e283194534cc1e2018509fb72475d0a3f725]]]]]
    Former-commit-id: 86f5742e4d1ac43904a1794744b5aa6aad9ce7a3
    Former-commit-id: cda8bcdb1077eb0504d4fca9b20c81992837fda8
    Former-commit-id: b7e829e7a9460dc145146c0cbce3a18d2034b621
    Former-commit-id: f5877d9bd0eee27a74520eae02a34ce1a6a79944
    Former-commit-id: 3bbb532d7b1001022c25496eb34d2f4a3c40c86f
    Former-commit-id: 7d63b644bdf3f44012f4e7d947b68a021346f628

--- src/render/ImageMapRenderer.java
+++ src/render/ImageMapRenderer.java
@@ -3,6 +3,7 @@ package de.stylextv.gs.render;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.map.MapCanvas;
 import org.bukkit.map.MapRenderer;
@@ -18,10 +19,22 @@ public class ImageMapRenderer extends MapRenderer {
 		this.image=image;
 	}
 	
+	public boolean shouldDrawTo(Player p, ItemFrame frame) {
+		if(p.getLocation().distanceSquared(frame.getLocation())<30*30) {
+			if(playersDrewTo.contains(p)) {
+				return false;
+			}
+			playersDrewTo.add(p);
+			return true;
+		} else {
+			playersDrewTo.remove(p);
+			return false;
+		}
+	}
+	
 	@Override
 	public void render(MapView view, MapCanvas canvas, Player p) {
-		if(!playersDrewTo.contains(p)) {
-			playersDrewTo.add(p);
+		if(!Thread.currentThread().getName().equals("Server thread")) {
 			canvas.drawImage(0, 0, image);
 		}
 	}
