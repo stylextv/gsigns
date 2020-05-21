70be6d7f63ed60a06365a53e3fff8d6e1d9fd296
StylexTV <56502218+StylexTV@users.noreply.github.com>
2020-05-21 11:12:47 +0200

    Update 4.0
    Former-commit-id: 63f638bebd56798c63ca134af5bc2539e6815ec5 [formerly 401460d0ea54f3ccc9ccafc8b164dbb280b96477] [formerly 56a460faf7e68de79cf33910eea8379c31ecd729 [formerly ded9fff063e3540263dc304e4201081f26e96c32 [formerly ded9fff063e3540263dc304e4201081f26e96c32 [formerly ded9fff063e3540263dc304e4201081f26e96c32 [formerly c05c5c8ac21db4fb04675254c760b961fbafd556]]]]]
    Former-commit-id: 04a0b1db84d5a8cccd9695b72c2ba79be9b99252
    Former-commit-id: 7b7b873e7fb83183720ea5920d5ff373ecdd451a
    Former-commit-id: 4ea040cd5629671bf12613c3463adad55b4d286f
    Former-commit-id: 64e9f0e8823dbbd70752718dab653594c352dd3f
    Former-commit-id: d77ff9ce26ac7148fdf5e95e075d9767aa5df013
    Former-commit-id: da82525a79f6e6dbb16982486f6bd0d1ff4a6cae

--- src/de/stylextv/gs/event/EventPlayerInteract.java
+++ src/de/stylextv/gs/event/EventPlayerInteract.java
@@ -1,6 +1,7 @@
 package de.stylextv.gs.event;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 
@@ -8,7 +9,7 @@ import de.stylextv.gs.player.PlayerManager;
 
 public class EventPlayerInteract implements Listener {
 	
-	@EventHandler
+	@EventHandler(priority = EventPriority.LOWEST)
 	public void onPlayerInteract(PlayerInteractEvent e) {
 		PlayerManager.onPlayerInteract(e);
 	}
