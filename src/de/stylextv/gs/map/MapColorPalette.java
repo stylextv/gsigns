0626302de3de65972eca5a57a164f6e08aa28b0e
StylexTV <56502218+StylexTV@users.noreply.github.com>
2020-08-10 22:35:26 +0200

    Update 4.5
    Former-commit-id: 27391efd2e5a3b9fbe7426dc2ba3dfe9876cf547
    Former-commit-id: db5bf8105b2d69994c5e640507652532afdf482e
    Former-commit-id: f9912f97ee7c24822d63e0ec12adf96a96a7025a
    Former-commit-id: 31f262e4a8a75edf4dd3d4c8e52ee10f332da123
    Former-commit-id: 453e91dab630786177557740add6e88a63161b8d
    Former-commit-id: 3a439948849a69dc94c7b665c7f7be6619bfd914
    Former-commit-id: a075e649cbb3ce417bd4a4b406af8c3cfef19aa8

--- src/de/stylextv/gs/map/MapColorPalette.java
+++ src/de/stylextv/gs/map/MapColorPalette.java
@@ -13,6 +13,7 @@ public class MapColorPalette {
         try {
             String path = "/assets/color_tables/1_12.colors";
             if(WorldUtil.getMcVersion()<WorldUtil.MCVERSION_1_12) path="/assets/color_tables/1_8_8.colors";
+            else if(WorldUtil.getMcVersion()>=WorldUtil.MCVERSION_1_16) path="/assets/color_tables/1_16.colors";
             MCSDBubbleFormat bubbleData = new MCSDBubbleFormat();
             
             InputStream input = MapColorPalette.class.getResourceAsStream(path);
@@ -25,13 +26,37 @@ public class MapColorPalette {
         } catch(Exception ex) {
             ex.printStackTrace();
         }
+        
+//        MCSDBubbleFormat data=generateBukkitTable();
+//        try {
+//			data.writeTo(new FileOutputStream("table_out.colors"));
+//		} catch (IOException ex) {ex.printStackTrace();}
     }
+//    @SuppressWarnings("deprecation")
+//	private static MCSDBubbleFormat generateBukkitTable() {
+//    	MCSDBubbleFormat data=new MCSDBubbleFormat();
+//    	data.clear();
+//        for (int i = 0; i < 256; i++) {
+//            try {
+//            	data.setColor((byte) i, MapPalette.getColor((byte) i));
+//            } catch (Throwable t) {}
+//        }
+//        for (int r = 0; r < 256; r++) {
+//            System.out.println("Generating Bukkit color map " + (r + 1) + "/256");
+//            for (int g = 0; g < 256; g++) {
+//                for (int b = 0; b < 256; b++) {
+//                	data.set(r, g, b, MapPalette.matchColor(r, g, b));
+//                }
+//            }
+//        }
+//		return data;
+//    }
 	
-    public static byte getColor(int r, int g, int b) {
+	public static byte getColor(int r, int g, int b) {
         return COLOR_MAP_DATA.get(r, g, b);
     }
-    public static Color getColor(byte b) {
-        return COLOR_MAP_DATA.getColor(b);
+	public static Color getColor(byte b) {
+    	return COLOR_MAP_DATA.getColor(b);
     }
 	
 }
