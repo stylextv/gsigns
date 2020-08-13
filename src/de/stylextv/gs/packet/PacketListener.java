21d1d2d79883368c4cc0c4075025604a511c1eca
StylexTV <56502218+StylexTV@users.noreply.github.com>
2020-08-13 17:30:11 +0200

    Update 4.7
    Former-commit-id: 956775c57a8d3c1dde1199bd83b6a98cc91f6758
    Former-commit-id: 57c37c868e0ce6e42c05709d5ff3090e678a9d44
    Former-commit-id: c9fdb490c3a7fd17120f5d0f5c08fcd8e94f967a
    Former-commit-id: ecc41c74ffc187d314fcb743d15af14ca9e6ac01
    Former-commit-id: 91819bf1c481dad7e259faf7e0a4b59f8fee0800
    Former-commit-id: 284d771cb95c812c5f8001da1626e8aa27e1907e
    Former-commit-id: 00502867253ad8feca1ec9d41a7cb4789124cdf9

---
+++ src/de/stylextv/gs/packet/PacketListener.java
@@ -0,0 +1,44 @@
+package de.stylextv.gs.packet;
+
+import org.bukkit.entity.Player;
+
+import de.stylextv.gs.packet.Reflections.FieldAccessor;
+import de.stylextv.gs.world.WorldUtil;
+
+/**
+ * @author Jitse Boonstra
+ */
+public class PacketListener {
+	
+    // Classes:
+    private final Class<?> packetPlayInUseEntityClazz = Reflections.getMinecraftClass("PacketPlayInUseEntity");
+    private final Class<?> enumEntityUseActionClazz = Reflections.getMinecraftClass("PacketPlayInUseEntity$EnumEntityUseAction");
+    
+    // Fields:
+    private final FieldAccessor<Integer> entityIdField = Reflections.getField(packetPlayInUseEntityClazz, "a", int.class);
+    private final FieldAccessor<?> interactTypeField = Reflections.getField(packetPlayInUseEntityClazz, "action", enumEntityUseActionClazz);
+    
+    public void start() {
+        new TinyProtocol() {
+            @Override
+            public Object onPacketInAsync(Player player, Object packet) {
+                return handleInteractPacket(player, packet) ? super.onPacketInAsync(player, packet) : null;
+            }
+        };
+    }
+    
+    private boolean handleInteractPacket(Player player, Object packet) {
+        if (!packetPlayInUseEntityClazz.isInstance(packet))
+            return true; // We aren't handling the packet.
+        
+        int packetEntityId = (int) entityIdField.get(packet);
+        boolean attack = interactTypeField.get(packet).toString().equals("ATTACK");
+        
+        if(!attack&&WorldUtil.isFrame(packetEntityId)) {
+        	return false;
+        }
+        
+        return true;
+    }
+    
+}
