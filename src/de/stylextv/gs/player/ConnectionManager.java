a392482cd9dd27d77ab8716d0bfdb82862cdba93
StylexTV <56502218+StylexTV@users.noreply.github.com>
2020-09-20 11:10:44 +0200

    Update 5.2
    Former-commit-id: e2249e8a2a61970ac7e884c71d33180f13e97b2d [formerly 58e526b7cb7151b96b24ffc614c890204202875e] [formerly 73d505a890bb059803116872ed104953e1616131] [formerly 1185a1c30c08aa3fa876036afd21d077e46e64ed]
    Former-commit-id: 2619cccc271dd06776b1fa58336fd0637d51533d
    Former-commit-id: 50d6e3b1ed74d4c0e1581586df5505585d6ed022
    Former-commit-id: 17a86a9bd54be7cbaa2978ea77d69fb4c48e6fc8

--- src/de/stylextv/gs/player/ConnectionManager.java
+++ src/de/stylextv/gs/player/ConnectionManager.java
@@ -7,16 +7,18 @@ import org.bukkit.entity.Player;
 
 public class ConnectionManager {
 	
-	private static int SENDS_PER_2TICKS=35;
-	private static boolean TICK_OSCILLATOR;
+	private static int SENDS_PER_3TICKS=40;
+	private static int TICK_OSCILLATOR;
 	
 	private static HashMap<Player, Integer> sendCounts=new HashMap<Player, Integer>();
 	
 	public static void update() {
-		TICK_OSCILLATOR=!TICK_OSCILLATOR;
-		if(TICK_OSCILLATOR) for(Player all:Bukkit.getOnlinePlayers()) {
-			sendCounts.put(all, 0);
-		}
+		if(TICK_OSCILLATOR==2) {
+			TICK_OSCILLATOR=0;
+			for(Player all:Bukkit.getOnlinePlayers()) {
+				sendCounts.put(all, 0);
+			}
+		} else TICK_OSCILLATOR++;
 	}
 	
 	public static int canSend(Player p, int amount) {
@@ -24,12 +26,12 @@ public class ConnectionManager {
 		
 		Integer i=sendCounts.get(p);
 		if(i==null) i=0;
-		if(i+amount<=SENDS_PER_2TICKS) {
+		if(i+amount<=SENDS_PER_3TICKS) {
 			sendCounts.put(p, i+amount);
 			return amount;
 		} else {
-			sendCounts.put(p, SENDS_PER_2TICKS);
-			return SENDS_PER_2TICKS-i;
+			sendCounts.put(p, SENDS_PER_3TICKS);
+			return SENDS_PER_3TICKS-i;
 		}
 	}
 	public static void removePlayer(Player p) {
